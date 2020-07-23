be5e00b588ed6e40a58f158edf781bc32e94557b
Arnold Amasoh <amasoharnold@gmail.com>
2020-07-23 12:15:45 -1100

    adding files to github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from "./components/card-list/card-list.components";
+import SearchBox from './components/search-box/search-box.component'
+import Head from './components/header/head.component'
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: '',
+      name: 'arnold'
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((Response) => Response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  handleChange = e => this.setState({ searchField: e.target.value }, () => console.log(this.state))
+
+  render() {
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()))
+
+    return (
+      <div className="App">
+        <Head />
+        <SearchBox placeholder='search monster' handleChange={this.handleChange} />
+        <CardList monsters={filteredMonsters} />
+
+      </div>
+
+    );
+  }
 }
 
 export default App;
---
+++ src/components/header/head.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Head = () => (
+    <h2>Monsters-Rolodex</h2>
+)
+
+export default Head
