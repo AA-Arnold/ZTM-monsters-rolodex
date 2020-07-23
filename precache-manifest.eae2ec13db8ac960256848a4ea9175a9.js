4e8e5d2b36cc8db02ec760c26ff4a42d5fd11ba4
Arnold Amasoh <amasoharnold@gmail.com>
2020-07-23 12:13:46 -1100

    Updates

---
+++ precache-manifest.eae2ec13db8ac960256848a4ea9175a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e8ed5bb388728c463c6c36197277a0",
+    "url": "/ZTM-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fb1df96001105063e0d9",
+    "url": "/ZTM-monsters-rolodex/static/css/main.ae52880b.chunk.css"
+  },
+  {
+    "revision": "049dd21cdf1ecc039e0a",
+    "url": "/ZTM-monsters-rolodex/static/js/2.16e3d737.chunk.js"
+  },
+  {
+    "revision": "fb1df96001105063e0d9",
+    "url": "/ZTM-monsters-rolodex/static/js/main.2fd471ad.chunk.js"
+  },
+  {
+    "revision": "c2abc992328ced354d6d",
+    "url": "/ZTM-monsters-rolodex/static/js/runtime-main.1bbea272.js"
+  }
+]);
